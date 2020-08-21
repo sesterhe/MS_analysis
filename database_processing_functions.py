0d5433fea0b53060f9ec28af7b9b08a703fd3c6c
Fabian Sesterhenn <fabian.sesterhenn@epfl.ch>
2020-08-21 08:58:15 +0200

    updated database processing functions

--- database_processing_functions.py
+++ database_processing_functions.py
@@ -1,16 +1,16 @@
 
 
 import rstoolbox.io as rs
-import json 
+import json
 
 
 def generate_dict_from_fasta(infile,outfile):
     '''''
-    this function was written to generate a python dictionary from a uniprot fasta file. It will return a dictionary with the UPIDs as keys and the sequence as values, stored in a json file. 
+    this function was written to generate a python dictionary from a uniprot fasta file. It will return a dictionary with the UPIDs as keys and the sequence as values, stored in a json file.
 
     USAGE: generate_dict_from_fasta("/Users/fabian/Downloads/uniprot_sprot.fasta","output.json")
 
-    The json file can later be loaded again by: 
+    The json file can later be loaded again by:
     with open('output.json', 'r') as fp:
     data = json.load(fp)
     '''''
@@ -31,3 +31,9 @@ def generate_dict_from_fasta(infile,outfile):
 
 
 
+def convert_df_to_dict(df, key, value):
+    '''''
+    This function takes as input a dataframe and returns a dictionary of two selected columns. The values are organized as a list with possibly multiple members.
+    '''''
+    dic = {k: list(set(g[value].tolist())) for k,g in df.groupby(key)}
+    return dic
